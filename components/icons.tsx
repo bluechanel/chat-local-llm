1495976d6037211a7d8f9e3f09ceb34e86b402ea
wiley <bluechanel612@gmail.com>
2024-04-22 16:04:39 +0800

    图标随主题变化

--- components/icons.tsx
+++ components/icons.tsx
@@ -126,10 +126,10 @@ export const EditIcon: React.FC<IconSvgProps> = ({
 			{...props}
 		>
 			<path
-				fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+				fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
 				d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8"
 			/>
-			<polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+			<polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
 		</svg>
 	);
 };
